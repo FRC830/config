f91d8d87fbd1a1492c2a573002c98d1a62abeba6
830-R2 <R2@830.local>
2014-12-07 17:42:22 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import sys, os, argparse, importlib
+
+class Util:
+    def input(self, prompt=''):
+        try:
+            return raw_input(prompt)
+        except NameError:
+            return input(prompt)
+
+parser = argparse.ArgumentParser();
+parser.add_argument('scripts', nargs='*')
+parser.add_argument('-a', '--all', action='store_true')
+args = parser.parse_args()
+
+files = args.scripts
+valid = map(lambda x: x.replace('.py', ''),
+        filter(lambda f: f.endswith('.py') and not f.startswith('__'),
+        os.listdir('config')))
+
+run = []
+if args.all:
+    run = valid
+else:
+    for f in files:
+        if not f in valid:
+            raise ValueError(f + ': not found')
+        run.append(f)
+
+for f in run:
+    print('* Running script %s' % f)
+    module = importlib.import_module('config.%s' % f)
+    if not hasattr(module, 'run'):
+        print('Missing run()')
+        continue
+    module.util = Util()
+    module.run()
+
+    
---
+++ config/git.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def conf(name):
+    try:
+        return subprocess.check_output('git config --global'.split() + [name])\
+               .replace('\n', '').replace('\r', '')
+    except subprocess.CalledProcessError:
+        return ''
+
+def set_conf(name, value):
+    subprocess.call('git config --global'.split() + [name, value])
+
+def run():
+    if not conf('user.name'):
+        username = util.input('Computer name (e.g. R2, B3): ')
+        set_conf('user.name', '830-%s' % username)
+        set_conf('user.email', '%s@830.local' % username)
+    set_conf('alias.stat', 'status')
+    set_conf('alias.st', 'status -s')
+    set_conf('alias.co', 'checkout')
+    
